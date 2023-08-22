804144cbd70c1dc10fd30c829fc7ca2dd82c7ccb
aneel2023 <developer.anil88@gmail.com>
2023-08-22 17:07:09 +0500

    print dir

--- pre_commit_hooks/magento_phpcs.py
+++ pre_commit_hooks/magento_phpcs.py
@@ -32,10 +32,22 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     )
     args = parser.parse_args(argv)
 
-    print(f'{module.match('magento/app/code/*/*')}: Root Path1')
-    print(f'{module.match('/magento/app/code/*/*')}: Root Path2')
-    print(f'{module.match('*/magento/app/code/*/*')}: Root Path3')
-    print(f'{module.match('app/code/*/*')}: Root Path4')
+    print(f'{module')}: Root Path0')
+    if module.match('magento/app/code/*/*'):
+        print(f'{module}: Root Path1')
+
+    if module.match('/magento/app/code/*/*'):
+        print(f'{module}: Root Path2')
+
+    if module.match('*/magento/app/code/*/*'):
+        print(f'{module}: Root Path3')
+
+     if module.match('app/code/*/*'):
+        print(f'{module')}: Root Path4')
+
+     if module.match('*/*/magento/app/code/*/*'):
+             print(f'{module')}: Root Path5')
+
     if module.match('**/magento/app/code/*/*'):
         # path to the root of magento
         magento = module.parent.parent.parent.parent
